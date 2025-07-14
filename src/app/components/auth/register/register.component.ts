0f6b0c5da766259fe45942811e239bbdf1cf1d24
MHMD <115650241+281mohamed@users.noreply.github.com>
2025-07-15 00:13:00 +0300

    Fixed the register button to have a loading state spinner

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -78,4 +78,20 @@ export class RegisterComponent {
       });
     }
   }
+
+  scrollToWhyJoin() {
+  const element = document.getElementById('whyJoin');
+  if (element) {
+    const offset = 80;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
+  }
+}
 }
